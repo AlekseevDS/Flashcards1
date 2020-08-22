062f2b0a273925d1a5b6a4c81fe91d0b7fcd398c
AlekseevDS <alekseevdms@gmail.com>
2020-08-22 18:10:48 +0300

    add array find

---
+++ Problems/Two numbers never occur next to each other/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int len = scan.nextInt(); // The first line contains the size of an array.
+        int[] arr = new int[len];
+        int n;
+        int m;
+        boolean result = true;
+
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
+        n = scan.nextInt();
+        m = scan.nextInt();
+        ArrayList<Integer> placeOfN = searchList(arr, n);
+        ArrayList<Integer> placeOfM = searchList(arr, m);
+
+        for (int i = 0; i < placeOfN.size(); i++) {
+            for (int c = 0; c < placeOfM.size(); c++) {
+                if (placeOfN.get(i) == placeOfM.get(c) + 1 || placeOfN.get(i) == placeOfM.get(c) -1) {
+                    result = false;
+                    break;
+                }
+            }
+
+        }
+        System.out.println(result);
+    }
+    public static ArrayList<Integer> searchList(int[] arrName, int num) {
+        ArrayList<Integer> place = new ArrayList<Integer>();
+        int count = -1;
+        for(int i: arrName) {
+            count = count + 1;
+            if (i == num) {
+                place.add(count);
+            }
+        }
+        return place;
+    }
+}
